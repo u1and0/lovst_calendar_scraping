0070f2cb38db42cfc11c6e364240cdd824ac67d3
u1and0 <e01.ando60@gmail.com>
2025-01-25 11:19:33 +0900

    [refactor] POSTメッセージの修正

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import asyncio
-from date_scraper import get_all_reservations
+from date_scraper import get_all_reservations, BASE_URL
 from link_forest_calendar.line import line_post
 
 
 async def main():
     all_reserve = await get_all_reservations(
         "https://reserve.lovstmade.com/reserve/calendar/115/202/261")
+    # print("デバッグ用 repr:", repr(all_reserve))
 
-    debug = False
-
-    if debug:
-        print("デバッグ用 repr:", repr(all_reserve))
-
-    # 標準出力
     print(all_reserve)
-    line_post(all_reserve)
+    message = f"""
+Lovst Photo Studio 予約フォーム {BASE_URL}
+からモデルオーディション予約組数を集計しました。
+
+{all_reserve}
+"""
+    line_post(message)
 
 
 asyncio.run(main())
